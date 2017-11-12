b14fc2b9b974ac7855baed3d6d21fb19e2f39dbb
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-12 23:53:57 +0000

    minor js error, small error

--- main.js
+++ main.js
@@ -22,6 +22,16 @@ $(document).ready(function(){
     // call my function randomNumber
     randomNumber();
     var i = 0;
+    var interval = setInterval(function () {
+      var id = computerSequence[i];
+      var color = $("#"+id).attr("class");
+      console.log("This is the id:"+id+" this is the color:"+color);
+      addingTempClass(id,color);
+      i++;
+      if (i === computerSequence.length){
+        clearInterval(interval);
+      }
+    }, 1000);
   }
 
   // function that uses math.random to get random sequence.
@@ -29,6 +39,14 @@ $(document).ready(function(){
     var randNum = Math.floor(Math.random() * 4);
     // push random number to computerSequence
     computerSequence.push(randNum);
+  }
+
+  // add class in the start sequence
+  function addingTempClass(id,color) {
+    $("#"+id).addClass("on");
+    setTimeout(function({
+      $("#"+id).removeClass("on");
+    },500)
   }
 
 
