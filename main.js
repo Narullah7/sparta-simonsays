8ef2f38d0d29dbed911f974ba7d3070995d71fe6
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-12 00:22:10 +0000

    initial commit + basic styling of html page

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  console.log("ready");
+
+// global variables
+
+//function which adjusts the lives
+
+// click event on the 4 buttons which generates a random color sequence (maybe using math.random())
+
+
+//function which checks for winner, loser and increments the score and lives
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
