92b4ee6fff8b9a1736af7b30e18f2fb084957985
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-12 20:15:31 +0000

    addded a start game into html to use in js

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
   console.log("ready");
 
 // global variables
+var userSequence = [];
+var computerSequence = [];
 
 //function which adjusts the lives
 
 // click event on the 4 buttons which generates a random color sequence (maybe using math.random())
-
+function randomSequence(){
+  var random = Math.floor(Math.random() * 4);
+  computerSequence.push();
+}
 
 //function which checks for winner, loser and increments the score and lives
 
